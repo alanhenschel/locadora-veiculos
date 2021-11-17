070286268140dfd8d7d758d7075fdf0edee4621c
alan <alan_henschel@hotmail.com>
2021-11-16 23:05:10 -0300

    feature: adding first persistence interface

---
+++ src/LocadoraVeiculos.Persistence/interfaces/IDefaultPersistence.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Persistence.interfaces
+{
+    public interface IDefaultPersistence
+    {
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        void DeleteRange<T>(T[] entity) where T : class;
+        Task<bool> SaveChangesAsync();
+    }
+}
